6fa2c22d90bf052a14cecf672a6d039ccdcea48c
sejin8839 <sejinon01@gmail.com>
2023-05-11 19:48:17 +0900

    Update assignment7_p2.cpp

--- assignment7_p2.cpp
+++ assignment7_p2.cpp
@@ -1,17 +1,17 @@
 /*
 file name: Assignment7_p2.cpp
-Written by: 
-Date:
+Written by: Sejin Yoon
+Date: May 11, 2023
 */
 
 
 #include<iostream>
 using namespace std;
+
 /**
 Every Dimension object is specified in two parts centimeters and millimeters.
 that is CM and MM
 **/
-
 class Dimension
 {
     private:
@@ -20,29 +20,33 @@ class Dimension
     public:
         void input()
         {
-            
-            // Student TODOS
+            cout << "Enter centimeters: ";
+            cin >> CM;
+            cout << "Enter millimeters: ";
+            cin >> MM;
         }
         void output()
         {
-             // Student TODOS
+            cout << CM << " cm " << MM << " mm" << endl;
         }
-    Dimension add(Dimension d)
-    {
-
-        // Student TODOS
-
-    }
+        Dimension add(Dimension d)
+        {
+            Dimension result;
+            result.MM = (MM + d.MM) % 10;
+            result.CM = CM + d.CM + (MM + d.MM) / 10;
+            return result;
+        }
 };
+
 int main()
 {
-    Dimension L1,L2,total;
-    
-    cout<<"*Enter Line Segment 1 Dimension (centimetres and millimetres): ";
+    Dimension L1, L2, total;
+    cout << "*Enter Line Segment 1 Dimension (centimetres and millimetres): ";
     L1.input();
-   cout<<"*Enter Line Segment 2 Dimension (centimetres and millimetres): ";
+    cout << "*Enter Line Segment 2 Dimension (centimetres and millimetres): ";
     L2.input();
-    total=L1.add(L2);
-    cout<<"Dimension: L1 + L2 = ";
+    total = L1.add(L2);
+    cout << "Dimension: L1 + L2 = ";
     total.output();
+    return 0;
 }
